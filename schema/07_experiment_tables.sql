b64546c0326414a5fe14ee16723fae7d2a88b200
James Hartley <james.ag.hartley@gmail.com>
2025-05-29 17:32:57 +0100

    feat(schma): updated the experiments table to store the number of predictions and time taken.

--- schema/07_experiment_tables.sql
+++ schema/07_experiment_tables.sql
@@ -1,16 +1,21 @@
 CREATE TABLE prediction_experiments (
     experiment_key TEXT PRIMARY KEY,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    metadata JSONB
+    metadata JSONB,
+    started_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    completed_at TIMESTAMP WITH TIME ZONE,
+    total_predictions INTEGER NOT NULL DEFAULT 0,
+    total_products INTEGER NOT NULL DEFAULT 0,
+    average_time_per_prediction FLOAT
 );
 
 CREATE TABLE prediction_results (
     prediction_key TEXT PRIMARY KEY,
     experiment_key TEXT REFERENCES prediction_experiments(experiment_key),
-    product_key TEXT, -- REFERENCES raw_products(product_key)
-    attribute_key TEXT, -- REFERENCES raw_attributes(attribute_key)
+    product_key TEXT REFERENCES raw_products(product_key),
+    attribute_key TEXT REFERENCES raw_attributes(attribute_key),
     value TEXT,
     confidence FLOAT,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    recommendation_key TEXT -- REFERENCES raw_recommendations(recommendation_key)
+    recommendation_key TEXT REFERENCES raw_recommendations(recommendation_key)
 );
